5953563e390e3dd4274231a81f24de7abf48a4db
coinableS <coinables@gmail.com>
2015-11-24 21:34:46 -0800

    db edit
    db edit

--- db.sql
+++ db.sql
@@ -16,9 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `dailnoiq_farm`
---
+
 
 -- --------------------------------------------------------
 
