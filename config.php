f8b401afd0e9e24a2dfcff6274aff3f8edb858df
godbody93 <65567143+godbody93@users.noreply.github.com>
2020-05-26 03:47:12 -0500

    update
    update to site test

--- config.php
+++ config.php
@@ -6,22 +6,20 @@ $dbpw = "password";
 $db = "databse_Name";
 
 //blockchain info API Key and your xpub BIP32 public key
-$api_key = "your_blockchain_api_Key";
-$xpub = "xpubYour_extended_public_key";
-$secret = "your_secret"; //this can be anything you want
+$api_key = "Y2BhjljBRMdoWUeSQRq75RyHtlNJBd8FRLa1BT5oL2Vr3zaHGoumNnI8i87BJkhZ";
+$xpub = "xpub6BgyETiwKAAbFLS1so6AqCaoVzqmh3KNnxiZmExjwZPfwMxc7TBqnFiqGhVEvFSzQqMdnpnPQ3pRGzqiRWgRCk39XnvkccQnR6te63cPps4";
+$secret = "Hell Mary, All Eyes On Me"; //this can be anything you want
 
 //Specific to you the store owner
-$storeName = "Shop Name";
-$rootURL = "http://yourrooturl.com/directory"; //example https://mysite.org  or http://yourhomepage.com/store
-$yourEmail = "your@email.com";  //email notifications will be sent to this email when a new order is placed
+$storeName = "BlocMoney";
+$rootURL = "https://github.com/godbody93/Blockchain-Receive-Payments-API-Shopping-Cart"; //example https://mysite.org  or http://yourhomepage.com/store
+$yourEmail = "ladon.loggins@gmail.com";  //email notifications will be sent to this email when a new order is placed
 
 //pw to access the admin pages
-$adminPW = "honeybadgerbubblegum"; 
+$adminPW = "Hell Mary, All Eyes On Me"; 
 
 //connect to the database
 $conn = mysqli_connect("localhost", $dbuser, $dbpw, $db);
 if(!$conn){
   die('Connection Error: '.mysqli_errno());
 }
-
-?>
